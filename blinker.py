2e4203d459fcd3b8ebe27b4837a2328dc9079c34
Eric Cattoir <cattoire@Macdroid.home>
2019-11-01 18:37:18 +0100

    updated to 2 led blinker

--- blinker.py
+++ blinker.py
@@ -3,9 +3,13 @@ from time import sleep     # Import the sleep function from the time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial
-    
+GPIO.setup(10, GPIO.OUT, initial=GPIO.HIGH)
+
 while True: # Run forever
     GPIO.output(8, GPIO.HIGH) # Turn on
+    GPIO.output(10, GPIO.LOW)
     sleep(1)                  # Sleep for 1 second
+    
     GPIO.output(8, GPIO.LOW)  # Turn off
+    GPIO.output(10, GPIO.HIGH)
     sleep(1)                  # Sleep for 1 second value to low (off)
