2f225647d089fb27b961b0df980471a55cc13c32
Eric Cattoir <cattoire@Macdroid.home>
2019-11-01 19:17:45 +0100

    initial value set

--- blinker.py
+++ blinker.py
@@ -3,7 +3,7 @@ from time import sleep     # Import the sleep function from the time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial
-GPIO.setup(10, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
 
 while True: # Run forever
     GPIO.output(8, GPIO.HIGH) # Turn on
